da3670b0f3cf4ac549bafcc4296d0ef47fc5853a
John Ragan <john.ragan@gmail.com>
2020-09-04 13:38:38 -0400

    Added more comments to this example.

--- src/App.test.js
+++ src/App.test.js
@@ -7,12 +7,20 @@ import App, { SearchNoSideEffectsOrState } from "./App";
 
 jest.mock("axios");
 
+// IGNORE THE RED CONSOLE WARNINGS - I JUST HAVE NOT HAD TIME TO LOOK INTO THIS
+// Do a "yarn start" to see app itself - less important - App.js
+// Uses "yarn test"  - App.test.js
+
+// You can see that it leverages Jest for BDD and expectations; RTL is more
+// focused on directly interrogating the actual DOM tree the user sees as
+// opposed to the React component structure.
 describe("App", () => {
   test("renders App component 1", () => {
     act(() => {
       render(<App />);
     });
 
+    // Exact text or RegEx matching.  But if not found, will throw an exception
     expect(screen.getByText("Search:")).toBeInTheDocument();
     expect(screen.getByText(/Search:/)).toBeInTheDocument();
   });
@@ -20,6 +28,8 @@ describe("App", () => {
   test("test selecting via role for test approach", () => {
     render(<App />);
 
+    // If you have inputs with same label, it will throw an exception.  You can
+    // try this by changing "Surch:" to "Search": in App.js line 57.
     screen.getByText("Search:");
   });
 
@@ -28,53 +38,53 @@ describe("App", () => {
       render(<App />);
     });
 
-    //screen.debug();
+    // Screen works with the DOM structure the user sees on the screen, as opposed to
+    // the React component structure.  Uncomment the next line to see that structure:
 
-    // getByText would fail before the toBeNull() check
+    // screen.debug();
+
+    // getByText would fail before the toBeNull() check because this component does not exist.
     expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
   });
 
   test("renders App component 3", async () => {
     render(<App />);
 
+    // queryByText will not fail if the item does not exist
     expect(screen.queryByText(/Signed in as/)).toBeNull();
 
-    //screen.debug();
-
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
-
-    //screen.debug();
   });
 
+  // Here, we mimic searching for "Javascript"
   test("renders App component 4", async () => {
     render(<App />);
 
-    // wait for the user to resolve
+    // wait for the user to resolve, as it is a web call (async, await)
     // needs only be used in our special case
     await screen.findByText(/Signed in as/);
 
-    //screen.debug();
-
+    // Have not searched for it yet
     expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
 
+    // Fire searching for this as a Javascript event
     fireEvent.change(screen.getByLabelText("Search:"), {
       target: { value: "JavaScript" },
     });
 
+    // Now, it should show up in search results
     expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
-
-    //screen.debug();
   });
 
   test("renders App component 5", async () => {
     render(<App />);
 
-    // wait for the user to resolve
+    // wait for the user to resolve again
     await screen.findByText(/Signed in as/);
 
     expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
 
-    // A fireEvent.change() triggers only a change event whereas
+    // A fireEvent.change() triggers only a single change event whereas
     // userEvent.type triggers a change event, but also keyDown, keyPress,
     // and keyUp events.
     await userEvent.type(screen.getByLabelText("Search:"), "JavaScript");
@@ -82,6 +92,7 @@ describe("App", () => {
     expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
   });
 
+  // Mocking examples in test
   describe("SearchNoSideEffectsOrState - fireEvent", () => {
     test("calls the onChange callback handler", () => {
       const onChange = jest.fn();
@@ -162,6 +173,7 @@ describe("App", () => {
 
       await userEvent.type(screen.getByLabelText("Search2:"), "JavaScript");
 
+      //  It is 10 times because each key press counts, unlike fireEvent
       expect(onChange).toHaveBeenCalledTimes(10);
     });
   });
@@ -177,4 +189,5 @@ describe("App", () => {
 // And there is the last resort search type TestId with getByTestId where
 // one needs to assign data-testid attributes in the source code's HTML.
 
+// Good article below:
 // https://www.robinwieruch.de/react-testing-library
